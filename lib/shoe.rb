c3fc7e2f3016ea4a6d3584ae9dae466751fa1d4d
github username <github email address>
2018-07-19 12:09:48 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    # initalize tells us this is the arguement we are taking in
+     @brand = brand
+    # the arguement we placed in parathesis will now be aviaable
+    # throughout the CLASS
+
+      # if the arguement equals the variable
+    if BRANDS.include?(brand) == false
+      # if our array BRANDS doesnt inlcude the arguement
+        BRANDS << brand
+
+        # then add it to the array
+  end
+end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+
+
+end
